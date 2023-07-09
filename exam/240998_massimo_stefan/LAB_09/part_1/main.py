d2dad7277fb4c8203fc298ac4196f506d1722d92
Itakello <maxste000@gmail.com>
2023-07-09 15:41:21 +0200

    Start lab 09

--- exam/240998_massimo_stefan/LAB_09/part_1/main.py
+++ exam/240998_massimo_stefan/LAB_09/part_1/main.py
@@ -1,9 +1,6 @@
-# This file is used to run your functions and print the results
-# Please write your fuctions or classes in the functions.py
-
-# Import everything from functions.py file
 from functions import *
+from utils import *
+from model import *
 
 if __name__ == "__main__":
-    #Wrtite the code to load the datasets and to run your functions
-    # Print the results
+    train_loader, dev_loader, test_loader = get_dataloaders()
