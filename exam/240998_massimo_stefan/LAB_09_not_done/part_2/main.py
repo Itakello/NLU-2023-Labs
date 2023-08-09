22e2848f0f492f3a539a9d71d89568361faa76a0
itakello <maxste000@gmail.com>
2023-08-09 17:03:48 +0000

    Done part 2 lab 9

--- exam/240998_massimo_stefan/LAB_09_not_done/part_2/main.py
+++ exam/240998_massimo_stefan/LAB_09_not_done/part_2/main.py
@@ -3,16 +3,14 @@ from utils import *
 from model import *
 
 if __name__ == "__main__":
-    models = {
-        LM_LSTM_Adv:{"emb_size": 300, "hidden_size": 300, "optimizer": "AdamW", "lr": 0.0001},
-        LM_LSTM_Adv:{"emb_size": 400, "hidden_size": 400, "optimizer": "AdamW", "lr": 0.0001},
-        LM_LSTM_Adv:{"emb_size": 500, "hidden_size": 500, "optimizer": "AdamW", "lr": 0.0001},
-    }
+    models = [
+        (LM_LSTM_Adv,{"emb_size": 400, "hidden_size": 400, "optimizer": "AdamW", "lr": 0.0001})
+    ]
     vocab_len = get_vocab_len()
     pad_index = get_pad_index()
     criterion_train = nn.CrossEntropyLoss(ignore_index=pad_index)
     criterion_eval = nn.CrossEntropyLoss(ignore_index=pad_index, reduction='sum')
-    for model, hyp in models.items():
+    for model, hyp in models:
         train_loader, dev_loader, test_loader = get_dataloaders()
         model = model(hyp['emb_size'], hyp['hidden_size'], vocab_len, pad_index=pad_index).to('cuda:0')
         model.apply(init_weights)
